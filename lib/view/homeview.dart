1140893388c926c00cdbf65c8f6d0503c5fde819
PubSec <pubsecs@proton.me>
2024-12-11 18:18:52 +0200

    scanner works

--- lib/view/homeview.dart
+++ lib/view/homeview.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
+import 'package:qr_code_dart_scan/qr_code_dart_scan.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -13,33 +13,29 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    String result = 'Scan failed';
+    // String scanResult = 'No result';
     return Scaffold(
       appBar: AppBar(
         title: const Text('QRScanner'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            String? res = await SimpleBarcodeScanner.scanBarcode(
-              context,
-              barcodeAppBar: const BarcodeAppBar(
-                appBarTitle: 'QR Scanner',
-                enableBackButton: true,
-                backButtonIcon: Icon(Icons.arrow_back_ios_new),
-              ),
-              cameraFace: CameraFace.back,
-              isShowFlashIcon: true,
-            );
-            setState(
-              () {
-                result = res as String;
-              },
-            );
-            print(result);
-          },
-          child: const Text('Scan'),
-        ),
+      body: QRCodeDartScanView(
+        takePictureButtonBuilder: (context, controller, isLoading) {
+          // if typeScan == TypeScan.takePicture you can customize the button.
+          if (isLoading) return const CircularProgressIndicator();
+          return ElevatedButton(
+            onPressed: controller.takePictureAndDecode,
+            child: const Text('Take a picture'),
+          );
+        },
+        typeScan: TypeScan.takePicture,
+        onCapture: (Result result) {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return Dialog(child: Text(result.text));
+            },
+          );
+        },
       ),
     );
   }
